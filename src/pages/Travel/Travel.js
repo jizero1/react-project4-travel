57ae68741d5fa860be2dc78bf6d6a33a74841f99
최지영 <jiyoung@choejiyeong-ui-MacBookAir.local>
2025-05-16 16:48:56 +0900

    filter에서 클릭한걸 travel.js에 전달함

--- src/pages/Travel/Travel.js
+++ src/pages/Travel/Travel.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './Travel.css';
 import travelData from './travelData.json';
@@ -9,25 +9,31 @@ function Travel() {
     const viewMoreClick = (id) => {
         navigate(`/travel/${id}`);
     }
+
+    const [filterClickData, setFilterClickData] = useState(0);
+    const filterBtnClick = (data) => {
+        setFilterClickData(data);
+        console.log(data);
+    }
     return (
         <div className="travel common-flex">
             {/* 여행지이름, 사진, 여행추천시기, 강수량, 여행난이도,
             환율(1달러=1350원), 여행지소개, 주요관광명소, 음식추천,
             전압 및 콘센트, 여행자 팁(팁문화, 사기조심 하라고 알려주는 등) */}
             {/* 여행하기 쉽다면 왜 쉬운지(언어,치안, 의료 등등), 여행 목적별 추천하는 시기도 보여줌 */}
-        <TravelFilter/>
-        {travelData.map((travel) => (
-            <div key={travel.id} className="travel-card">
-                <img className="travel-img" src={process.env.PUBLIC_URL+travel.imgSrc} alt={travel.imgAlt}></img>
-                <div className="travel-short-discription">
-                    <p>{travel.name} <span>[{travel.country}]</span></p>
-                    <p>{travel.discription}</p>
-                    <p><span>여행하기 좋은 달 </span>{travel.bestMonths}</p>
-                    <p><span>여행 난이도 </span>{travel.difficulty}</p>
+            <TravelFilter filterBtnClick={filterBtnClick} />
+            {travelData.map((travel) => (
+                <div key={travel.id} className="travel-card">
+                    <img className="travel-img" src={process.env.PUBLIC_URL + travel.imgSrc} alt={travel.imgAlt}></img>
+                    <div className="travel-short-discription">
+                        <p>{travel.name} <span>[{travel.country}]</span></p>
+                        <p>{travel.discription}</p>
+                        <p><span>여행하기 좋은 달 </span>{travel.bestMonths}</p>
+                        <p><span>여행 난이도 </span>{travel.difficulty}</p>
+                    </div>
+                    <p className="travel-viewMore" onClick={() => viewMoreClick(travel.id)}>► 더 자세히 보기</p>
                 </div>
-                <p className="travel-viewMore" onClick={() => viewMoreClick(travel.id)}>► 더 자세히 보기</p>
-            </div>
-        ))}
+            ))}
 
         </div>
     )
--- src/pages/Travel/TravelFilter.js
+++ src/pages/Travel/TravelFilter.js
@@ -1,11 +1,26 @@
 import React, { useState } from 'react';
 import './TravelFilter.css';
+import { MdFilterList, MdFilterListAlt } from "react-icons/md";
 
-function TravelFilter() {
+function TravelFilter({filterClick, filterBtnClick}) {
+
+    const [isListClick, setIsListClick] = useState(false);
+    const listClick = () => {
+        setIsListClick(!isListClick);
+    }
     return (
         <div className="travelFilter">
             {/* 추천순, 난이도순, 물가저렴한 순 */}
-            <div>추천순</div>
+            
+            <div className="filterBtn common-flex" onClick={() => listClick()}><MdFilterList />추천순</div>
+            <div className="filterHiddenBox" style={{display: isListClick ? 'block' : 'none'}}>
+                <ul>
+                    <li onClick={() => {filterBtnClick(0); listClick();}}>추천순</li>
+                    <li onClick={() => {filterBtnClick(1); listClick();}}>여행 난이도순</li>
+                    <li onClick={() => {filterBtnClick(2); listClick();}}>물가 저렴한순</li>
+                    <li onClick={() => {filterBtnClick(3); listClick();}}>물가 비싼순</li>
+                </ul>
+            </div>
         </div>
     )
 }
